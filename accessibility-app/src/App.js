352281b049917b996973a5ea60d0b57ebf5a7f2e
“MARIA <mdfortino@gmail.com>
2019-03-13 15:43:06 -0400

    install axe, test ininitial drop down

--- accessibility-app/src/App.js
+++ accessibility-app/src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className="App-header">
+        accessibility 
+      </header>
+      <div role="navigation" aria-label="Primary">
+      <nav> 
+      <ul className="menu">
+        <li className="menu__item">
+          <a href="/about" className="menu__link">learn more</a>
+          <ul className="submenu">
+            <li className="submenu__item">
+              <a className="submenu__link" href="/about/why">Why</a>
+            </li>
+            <li className="submenu__item">
+              <a className="submenu__link" href="/about/what">What</a>
+            </li>
+            <li className="submenu__item">
+              <a className="submenu__link" href="/about/how">How</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    </div>  
+    </div>
     );
   }
 }
